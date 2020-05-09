0eae73714999efce369c1e0ddd41d6e50cb57e6b
Veni13 <veni7678@gmail.com>
2020-05-09 18:09:23 +0530

    created the snake game

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
 
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+const World = Matter.World;
+
+var engine,world;
+var snake;
+var food;
+
 
 function setup(){
 var canvas = createCanvas(400,400);
+engine  = Engine.create();
+world = engine.world;
+snake = new Snake(200,200,30,30);
+food = new Food(300,200,50,50);
 }
 
 function draw(){
-    background(0);
+    background(255,255,255);
+    Engine.update(engine);
+    snake.display();
+    food.display();
 }
